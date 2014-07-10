82f8540d263ee6e60acea2a40fb671a869815696
Simon Wachter <simon@wachter.me>
2014-07-11 00:37:49 +0200

    Adds ranking, match mode, and compute mode (+/-)

--- js/scores.service.js
+++ js/scores.service.js
@@ -10,6 +10,9 @@ counterApp.factory('scoresService', function () {
     players.list = [];
     players.total_created = 0;
     players.turn_count = 0;
+    players.goal = 100;
+    players.match = 'off';
+    players.mode = 'add'
 
     /* Remove a player */
     players.remove = function (i) {
@@ -29,6 +32,43 @@ counterApp.factory('scoresService', function () {
       }
     };
 
+    players.rank = function (index) {
+      var over = 0;
+      var under = 0;
+      var equal = 0;
+
+      var ref = players.list[index].total;
+
+      /* Count those over and under the ref player */
+      for (var i=0; i < players.list.length; i++) {
+        if (i == index) continue;
+
+        var score = players.list[i].total;
+
+        if      (score < ref) under += 1;
+        else if (score > ref) over  += 1;
+        else                  equal += 1;
+      }
+
+      /* Depending on the mode and the match, we consider scores above or under the ref to
+       * make up the rank
+       */
+      var rank;
+      if ((players.mode == 'add' && (players.match == 'win' || players.match == 'off')) ||
+          (players.mode == 'substract' && players.match == 'lose'))
+        rank = over + 1;
+      else if ((players.mode == 'add' && players.match == 'lose') ||
+          (players.mode == 'substract' && (players.match == 'win' || players.match == 'off')))
+        rank = under + 1
+
+      // Get ordinal
+      // From: https://github.com/jdpedrie/angularjs-ordinal-filter
+      var s = ["th", "st", "nd", "rd"];
+      var v = rank % 100;
+
+      return (equal > 0 ? 'equal ' : '') + rank + ( s[(v - 20) % 10] || s[v] || s[0]);
+    };
+
     players.createPlayer = function () {
       var initscores = [];
 
@@ -39,10 +79,40 @@ counterApp.factory('scoresService', function () {
           "name": name + " " + players.total_created,
           "scores": initscores,
 
+          /** Check if the player lost **/
+          "_lost": function () {
+            if (players.match == 'win') {
+              return false;
+            } else if (players.match == 'lose') {
+              return player._total() >= players.goal;
+            }
+          },
+
+          /** Check if the player won **/
+          "_won": function () {
+            if (players.match == 'win') {
+              return player._total() >= players.goal;
+            } else if (players.match == 'lose') {
+              return false;
+            }
+          },
+
+          /** Get the player's score **/
           "_total": function () {
             var sum = 0;
-            for (var i = 0; i < player.scores.length; i++) {
-                sum += +player.scores[i];
+
+            // In adding mode, just sum the scores
+            if (players.mode == 'add') {
+              for (var i = 0; i < player.scores.length; i++) {
+                  sum += +player.scores[i];
+              }
+
+            // In substract mode, substract all scores from the first
+            } else if (players.mode == 'substract') {
+              sum = player.scores[0] || 0;
+              for (var i = 1; i < player.scores.length; i++) {
+                  sum -= +player.scores[i];
+              }
             }
 
             return sum;
@@ -58,13 +128,31 @@ counterApp.factory('scoresService', function () {
         player.__defineGetter__("total", function () {
           return player._total();
         });
+        player.__defineGetter__("won", function () {
+          return player._won();
+        });
+        player.__defineGetter__("lost", function () {
+          return player._lost();
+        });
       } else {
         Object.defineProperty(player, "total", {
-            writable: false,
-            enumerable: true,
-            get: function () {
-              return player._total();
-            }
+          writable: false,
+          enumerable: true,
+          get: function () {
+            return player._total();
+          }
+        });
+        Object.defineProperty(player, "won", {
+          writable: false,
+          get: function () {
+            return player._won();
+          }
+        });
+        Object.defineProperty(player, "lost", {
+          writable: false,
+          get: function () {
+            return player._lost();
+          }
         });
       }
 
